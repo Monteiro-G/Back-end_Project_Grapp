a0e6892d3d4b9a3dee9912684e4c04c484ee1de7
Gilberto_Moura <gylberto120.gm@gmail.com>
2023-06-21 23:29:27 -0300

    finalizado os serializers

--- mygrsite/backend/serializers.py
+++ mygrsite/backend/serializers.py
@@ -9,4 +9,54 @@ class ClienteSerializer(serializers.ModelSerializer):
 class PedidoCompraSerializer(serializers.ModelSerializer):
     class Meta:
         model = PedidoCompra
+        feilds = '__all__'
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+class MarcaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Marca
+        fields = '__all__'
+
+class FornecedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fornecedor
+        fields = '__all__'
+
+class EstoqueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estoque
+        fields = '__all__'
+
+class SecaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Secao
+        fields = '__all__'
+
+class CabecarioPedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CabecarioPedido
+        fields = '__all__'
+
+class ItensPedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItensPedido
+        fields = '__all__'
+
+class CobrancaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cobranca
+        fields = '__all__'
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
+
+class CidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cidade
         fields = '__all__'
